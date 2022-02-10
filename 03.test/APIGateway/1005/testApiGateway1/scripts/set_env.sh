dc681145057fb0d72875dac687b5b904de2adf20
Myhael76 <myhael76.noreply@nowhere.com>
2022-02-10 15:28:13 +0100

    Fix on SUIF_HOME default value for 1005 testApiGw1

--- 03.test/APIGateway/1005/testApiGateway1/scripts/set_env.sh
+++ 03.test/APIGateway/1005/testApiGateway1/scripts/set_env.sh
@@ -9,7 +9,7 @@ export SUIF_DEBUG_ON=1                      # we want to see details
 # expect these values to match the mounts
 export SUIF_AUDIT_BASE_DIR=${SUIF_AUDIT_BASE_DIR:-"/app/audit"}
 
-export SUIF_HOME=${SUIF_HOME."/mnt/SUIF_HOME"} # must have this, we are running offline
+export SUIF_HOME=${SUIF_HOME:-"/mnt/SUIF_HOME"} # must have this, we are running offline
 
 if [ ! -f "${SUIF_HOME}/01.scripts/installation/setupFunctions.sh" ]; then
     echo "PANIC! Working in offline mode, but SUIF dependency not present! declared SUIF_HOME is ${SUIF_HOME}"
