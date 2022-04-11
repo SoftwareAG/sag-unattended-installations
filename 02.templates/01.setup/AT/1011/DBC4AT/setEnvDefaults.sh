ec008e9341f0bb38c9fa8e7dae373fd60e28aafe
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2022-04-11 18:18:36 +0200

    DBC4AT 1011 added (untested)

---
+++ 02.templates/01.setup/AT/1011/DBC4AT/setEnvDefaults.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ ! "`type -t urlencode`X" == "functionX" ]; then
+    if [ ! -f "${SUIF_CACHE_HOME}/installationScripts/commonFunctions.sh" ]; then
+        echo "Panic, common functions not sourced and not present locally! Cannot continue"
+        exit 500
+    fi
+    . "$SUIF_CACHE_HOME/installationScripts/commonFunctions.sh"
+fi
+
+# Section 1 - the caller MUST provide
+## Framework - Install
+export SUIF_INSTALL_INSTALLER_BIN=${SUIF_INSTALL_INSTALLER_BIN:-"/path/to/installer.bin"}
+export SUIF_INSTALL_IMAGE_FILE=${SUIF_INSTALL_IMAGE_FILE:-"/path/to/install/product.image.zip"}
+
+## Framework - Patch
+export SUIF_PATCH_SUM_BOOSTSTRAP_BIN=${SUIF_PATCH_SUM_BOOSTSTRAP_BIN:-"/path/to/sum-boostrap.bin"}
+export SUIF_PATCH_FIXES_IMAGE_FILE=${SUIF_PATCH_FIXES_IMAGE_FILE:-"/path/to/install/fixes.image.zip"}
+
+## Current Template
+
+# Section 2 - the caller MAY provide
+## Framework - Install
+export SUIF_INSTALL_INSTALL_DIR=${SUIF_INSTALL_INSTALL_DIR:-"/opt/sag/products"}
+export SUIF_INSTALL_DECLARED_HOSTNAME=${SUIF_INSTALL_DECLARED_HOSTNAME:-"localhost"}
+## Framework - Patch
+export SUIF_SUM_HOME=${SUIF_SUM_HOME:-"/opt/sag/sum"}
+
+logI "Template environment sourced successfully"
+logEnv4Debug
