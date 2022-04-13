fec3811ae9cbbe5cc47504792bdcb8162fdd10d4
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2022-04-13 15:35:26 +0200

    added ats + cu on postgres

---
+++ 02.templates/01.setup/AT/1011/server/minimal-with-cu-on-postgresql/setEnvDefaults.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+if [ ! "`type -t urlencode`X" == "functionX" ]; then
+    if [ ! -f "${SUIF_CACHE_HOME}/installationScripts/commonFunctions.sh" ]; then
+        echo "Panic, common functions not sourced and not present locally! Cannot continue"
+        exit 500
+    fi
+    . "$SUIF_CACHE_HOME/installationScripts/commonFunctions.sh"
+fi
+
+########### Section 1 - the caller MUST provide
+## Framework - Install
+export SUIF_INSTALL_INSTALLER_BIN=${SUIF_INSTALL_INSTALLER_BIN:-"/path/to/installer.bin"}
+export SUIF_INSTALL_IMAGE_FILE=${SUIF_INSTALL_IMAGE_FILE:-"/path/to/install/product.image.zip"}
+
+## Framework - Patch
+export SUIF_PATCH_SUM_BOOSTSTRAP_BIN=${SUIF_PATCH_SUM_BOOSTSTRAP_BIN:-"/path/to/sum-boostrap.bin"}
+export SUIF_PATCH_FIXES_IMAGE_FILE=${SUIF_PATCH_FIXES_IMAGE_FILE:-"/path/to/install/fixes.image.zip"}
+
+# Licenses
+SUIF_SETUP_TEMPLATE_IS_LICENSE_FILE=${SUIF_SETUP_TEMPLATE_IS_LICENSE_FILE:-'No Is License File Provided'}
+export SUIF_WMSCRIPT_integrationServerLicenseFiletext=$(urlencode ${SUIF_SETUP_TEMPLATE_IS_LICENSE_FILE})
+
+SUIF_SETUP_TEMPLATE_MFTSERVER_LICENSE_FILE=${SUIF_SETUP_TEMPLATE_MFTSERVER_LICENSE_FILE:-'No Active Transfer License File Provided'}
+export SUIF_WMSCRIPT_MFTLicenseFile=$(urlencode ${SUIF_SETUP_TEMPLATE_MFTSERVER_LICENSE_FILE})
+
+# Integration server "Internal" DB connection
+export SUIF_WMSCRIPT_IntegrationServerDBPassName=${SUIF_WMSCRIPT_IntegrationServerDBPassName:-'isDbPass'}
+export SUIF_WMSCRIPT_IntegrationServerDBUserName=${SUIF_WMSCRIPT_IntegrationServerDBUserName:-'isDbUser'}
+export SUIF_WMSCRIPT_IntegrationServerPoolName=${SUIF_WMSCRIPT_IntegrationServerPoolName:-'isDbConnPool'}
+export SUIF_WMSCRIPT_IS_JDBC_CONN_STRING=${SUIF_WMSCRIPT_IS_JDBC_CONN_STRING:-'jdbc:wm:postgresql://postgres-server-is:5432;databaseName=isDbName'}
+export SUIF_WMSCRIPT_IntegrationServerDBURLName=$(urlencode ${SUIF_WMSCRIPT_IS_JDBC_CONN_STRING})
+
+# Active Transfer DB connection
+export SUIF_WMSCRIPT_ActiveServerPasswordName=${SUIF_WMSCRIPT_ActiveServerPasswordName:-'atsDbPass'}
+export SUIF_WMSCRIPT_ActiveServerPoolName=${SUIF_WMSCRIPT_ActiveServerPoolName:-'atsDbConnPool'}
+export SUIF_WMSCRIPT_ActiveServerUserName=${SUIF_WMSCRIPT_ActiveServerUserName:-'atsDbUser'}
+export SUIF_WMSCRIPT_ATS_JDBC_CONN_STRING=${SUIF_WMSCRIPT_ATS_JDBC_CONN_STRING:-'jdbc:wm:postgresql://postgres-server-ats:5432;databaseName=atsDbName'}
+export SUIF_WMSCRIPT_ActiveServerUrlName=$(urlencode ${SUIF_WMSCRIPT_ATS_JDBC_CONN_STRING})
+
+# Central Directory Services Connection
+export SUIF_WMSCRIPT_CDSConnectionName=${SUIF_WMSCRIPT_CDSConnectionName:-'cdsConnection'}
+export SUIF_WMSCRIPT_CDSPasswordName=${SUIF_WMSCRIPT_CDSPasswordName:-'cdsPassword'}
+export SUIF_WMSCRIPT_CDSUserName=${SUIF_WMSCRIPT_CDSUserName:-'cdsUser'}
+export SUIF_WMSCRIPT_CDS_JDBC_CONN_STRING=${SUIF_WMSCRIPT_CDS_JDBC_CONN_STRING:-'jdbc:wm:postgresql://postgres-server-cds:5432;databaseName=cdsDbName'}
+export SUIF_WMSCRIPT_CDSUrlName=$(urlencode ${SUIF_WMSCRIPT_CDS_JDBC_CONN_STRING})
+########### Section 1 END - the caller MUST provide
+
+########### Section 2 - the caller MAY provide
+## Framework - Install
+export SUIF_INSTALL_INSTALL_DIR=${SUIF_INSTALL_INSTALL_DIR:-"/opt/sag/products"}
+export SUIF_INSTALL_DECLARED_HOSTNAME=${SUIF_INSTALL_DECLARED_HOSTNAME:-"localhost"}
+## Framework - Patch
+export SUIF_SUM_HOME=${SUIF_SUM_HOME:-"/opt/sag/sum"}
+
+# Integration Server Administrator password
+export SUIF_WMSCRIPT_adminPassword=${SUIF_WMSCRIPT_adminPassword:-'manage01'}
+
+# Integration Server Ports
+export SUIF_WMSCRIPT_IntegrationServerdiagnosticPort=${SUIF_WMSCRIPT_IntegrationServerdiagnosticPort:-9999}
+export SUIF_WMSCRIPT_IntegrationServerPort=${SUIF_WMSCRIPT_IntegrationServerPort:-5555}
+export SUIF_WMSCRIPT_IntegrationServersecurePort=${SUIF_WMSCRIPT_IntegrationServersecurePort:-5543}
+
+# SPM Ports
+export SUIF_WMSCRIPT_SPMHttpPort=${SUIF_WMSCRIPT_SPMHttpPort:-8092}
+export SUIF_WMSCRIPT_SPMHttpsPort=${SUIF_WMSCRIPT_SPMHttpsPort:-8093}
+
+# Active Transfer Port
+export SUIF_WMSCRIPT_mftGWPortField=${SUIF_WMSCRIPT_mftGWPortField:-8500}
+########### Section 2 END - the caller MAY provide
+
+logI "Template environment sourced successfully"
+logEnv4Debug
---
+++ 03.test/ActiveTransfer/1011/ServerWithCuOnPostgreSQL1/scripts/containerEntrypoint.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+# This scripts sets up the local installation
+
+if [ ! -d ${SUIF_HOME} ]; then
+    echo "SUIF_HOME variable MUST point to an existing local folder! Current value is ${SUIF_HOME}"
+    exit 1
+fi
+
+# our configuration takes precedence in front of framework defaults, set it before sourcing the framework functions
+if [ ! -d "${SUIF_LOCAL_SCRIPTS_HOME}" ]; then
+    echo "Scripts folder not found: ${SUIF_LOCAL_SCRIPTS_HOME}"
+    exit 2
+fi
+
+# Source framework functions
+. "${SUIF_HOME}/01.scripts/commonFunctions.sh" || exit 4
+. "${SUIF_HOME}/01.scripts/installation/setupFunctions.sh" || exit 5
+
+# If the DBC installation is not present, do it now
+if [ ! -f "${SUIF_INSTALL_INSTALL_DIR_DBC}/common/db/bin/dbConfigurator.sh" ]; then
+    logI "Database configurator is not present, setting up ..."
+
+    export SUIF_INSTALL_IMAGE_FILE="${SUIF_INSTALL_IMAGE_FILE_DBC4AT}"
+    export SUIF_PATCH_FIXES_IMAGE_FILE="${SUIF_PATCH_FIXES_IMAGE_FILE_DBC4AT}"
+    export SUIF_INSTALL_INSTALL_DIR="${SUIF_INSTALL_INSTALL_DIR_DBC}"
+
+    # Parameters - applySetupTemplate
+    # $1 - Setup template directory, relative to <repo_home>/02.templates/01.setup
+    applySetupTemplate "AT/1011/DBC4AT" || exit 6
+fi
+
+createDbComponents(){
+
+    logI "Eventually creating DB components..."
+    sleep 5 # this container is starting too quickly...
+    local p=`portIsReachable ${SUIF_DBSERVER_HOSTNAME} ${SUIF_DBSERVER_PORT}`
+    while [ $p -eq 0 ]; do
+        logI "Waiting for the database to come up, sleeping 5..."
+        sleep 5
+        p=`portIsReachable ${SUIF_DBSERVER_HOSTNAME} ${SUIF_DBSERVER_PORT}`
+        # TODO: add an maximum retry number
+    done
+
+    export SUIF_INSTALL_INSTALL_DIR="${SUIF_INSTALL_INSTALL_DIR_DBC}"
+    # template specific parameters
+    applyPostSetupTemplate DBC/1011/postgresql-create
+}
+
+createDbComponents
+
+if [ ! -d "${SUIF_INSTALL_INSTALL_DIR_ATS}/IntegrationServer" ]; then
+    logI "Active Transfer Server not present, installing now..."
+    export SUIF_INSTALL_IMAGE_FILE="${SUIF_INSTALL_IMAGE_FILE_ATS}"
+    export SUIF_PATCH_FIXES_IMAGE_FILE="${SUIF_PATCH_FIXES_IMAGE_FILE_ATS}"
+    export SUIF_INSTALL_INSTALL_DIR="${SUIF_INSTALL_INSTALL_DIR_ATS}"
+
+    # Parameters - applySetupTemplate
+    # $1 - Setup template directory, relative to <repo_home>/02.templates/01.setup
+    applySetupTemplate "AT/1011/server/minimal-with-cu-on-postgresql" || exit 7
+fi
+
+onInterrupt(){
+	echo "Interrupted! Shutting Activetransfer Server..."
+    pushd . >/dev/null
+    cd "${SUIF_INSTALL_INSTALL_DIR_ATS}/profiles/IS_default/bin/"
+    ./shutdown.sh
+    popd >/dev/null
+	exit 0 # managed expected exit
+}
+
+onKill(){
+	logW "Killed!"
+}
+
+trap "onInterrupt" SIGINT SIGTERM
+trap "onKill" SIGKILL
+
+logI "Starting Active Transfer Server..."
+pushd . > /dev/null
+cd "${SUIF_INSTALL_INSTALL_DIR_ATS}/profiles/IS_default/bin/"
+./console.sh & wait
+
+popd > /dev/null
+
+if [ "${SUIF_DEBUG_ON}" -eq 1 ]; then
+  logW "Stopping for debug"
+  tail -f /dev/null
+fi
