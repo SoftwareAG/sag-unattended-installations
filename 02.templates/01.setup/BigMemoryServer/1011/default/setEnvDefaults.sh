4b09aefac293c9c9575af828b123d4376c198370
fabioromanapisciotta <fabioromanapisciotta@gmail.com>
2022-04-17 22:01:12 +0700

    test BigMemory 1011 plain (no fixes)

--- 02.templates/01.setup/BigMemoryServer/1011/default/setEnvDefaults.sh
+++ 02.templates/01.setup/BigMemoryServer/1011/default/setEnvDefaults.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+echo
+echo "calling setEnvDefaults.sh"
+echo
 # Depends on framework commons
 if [ ! "`type -t urlencode`X" == "functionX" ]; then
     echo "Need the function urlencode(), sourcing commonFunctions.sh "
@@ -18,8 +20,8 @@ export SUIF_INSTALL_IMAGE_FILE=${SUIF_INSTALL_IMAGE_FILE:-"/path/to/install/prod
 export SUIF_PATCH_SUM_BOOSTSTRAP_BIN=${SUIF_PATCH_SUM_BOOSTSTRAP_BIN:-"/path/to/sum-boostrap.bin"}
 export SUIF_PATCH_FIXES_IMAGE_FILE=${SUIF_PATCH_FIXES_IMAGE_FILE:-"/path/to/install/fixes.image.zip"}
 ## Current Template
-export SUIF_SETUP_TEMPLATE_TES_LICENSE_FILE=${SUIF_SETUP_TEMPLATE_TES_LICENSE_FILE:-"/provide/path/to/terracotta-license.key"}
-
+export SUIF_WMSCRIPT_TSALicenseFile=${SUIF_WMSCRIPT_TSALicenseFile:-"/provide/path/to/terracotta-license.key"}
+export SUIF_WMSCRIPT_TSALicenseFile_UrlEncoded=$(urlencode ${SUIF_WMSCRIPT_TSALicenseFile})
 # Section 2 - the caller MAY provide
 ## Framework - Install
 export SUIF_INSTALL_INSTALL_DIR=${SUIF_INSTALL_INSTALL_DIR:-"/opt/sag/products"}
@@ -31,4 +33,4 @@ export SUIF_SUM_HOME=${SUIF_SUM_HOME:-"/opt/sag/sum"}
 
 ## Section 3 - Post processing
 ## eventually provided values are overwritten!
-export SUIF_SETUP_TEMPLATE_TES_LICENSE_UrlEncoded=$(urlencode ${SUIF_SETUP_TEMPLATE_TES_LICENSE_FILE})
+
