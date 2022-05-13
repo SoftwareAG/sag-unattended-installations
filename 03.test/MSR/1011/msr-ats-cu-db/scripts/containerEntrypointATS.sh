97a322361e8ec21c10bf0f8a9e9c3996f0b96fad
fabioromanapisciotta <fabioromanapisciotta@gmail.com>
2022-05-13 15:56:27 +0700

    Completed msr-ats-cu-db test and updated MSR/1011/AdaptersSet1

--- 03.test/MSR/1011/msr-ats-cu-db/scripts/containerEntrypointATS.sh
+++ 03.test/MSR/1011/msr-ats-cu-db/scripts/containerEntrypointATS.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-
+#tail -f /dev/null
 # This scripts sets up the local installation
-
-
 
 if [ ! -d ${SUIF_HOME} ]; then
     echo "SUIF_HOME variable MUST point to an existing local folder! Current value is ${SUIF_HOME}"
@@ -19,9 +17,37 @@ fi
 . "${SUIF_HOME}/01.scripts/commonFunctions.sh" || exit 4
 . "${SUIF_HOME}/01.scripts/installation/setupFunctions.sh" || exit 5
 
+# If the DBC installation is not present, do it now
+if [ ! -f "${SUIF_INSTALL_INSTALL_DIR_DBC}/common/db/bin/dbConfigurator.sh" ]; then
+    logI "Database configurator is not present, setting up ..."
 
+    export SUIF_INSTALL_IMAGE_FILE="${SUIF_INSTALL_IMAGE_FILE_DBC4AT}"
+    export SUIF_PATCH_FIXES_IMAGE_FILE="${SUIF_PATCH_FIXES_IMAGE_FILE_DBC4AT}"
+    export SUIF_INSTALL_INSTALL_DIR="${SUIF_INSTALL_INSTALL_DIR_DBC}"
 
+    # Parameters - applySetupTemplate
+    # $1 - Setup template directory, relative to <repo_home>/02.templates/01.setup
+    applySetupTemplate "AT/1011/DBC4AT" || exit 6
+fi
 
+createDbComponents(){
+
+    logI "Eventually creating DB components..."
+    sleep 5 # this container is starting too quickly...
+    local p=`portIsReachable ${SUIF_DBSERVER_HOSTNAME} ${SUIF_DBSERVER_PORT}`
+    while [ $p -eq 0 ]; do
+        logI "Waiting for the database to come up, sleeping 5..."
+        sleep 5
+        p=`portIsReachable ${SUIF_DBSERVER_HOSTNAME} ${SUIF_DBSERVER_PORT}`
+        # TODO: add an maximum retry number
+    done
+
+    export SUIF_INSTALL_INSTALL_DIR="${SUIF_INSTALL_INSTALL_DIR_DBC}"
+    # template specific parameters
+    applyPostSetupTemplate DBC/1011/postgresql-create
+}
+
+createDbComponents
 
 if [ ! -d "${SUIF_INSTALL_INSTALL_DIR_ATS}/IntegrationServer" ]; then
     logI "Active Transfer Server not present, installing now..."
@@ -29,11 +55,6 @@ if [ ! -d "${SUIF_INSTALL_INSTALL_DIR_ATS}/IntegrationServer" ]; then
     export SUIF_PATCH_FIXES_IMAGE_FILE="${SUIF_PATCH_FIXES_IMAGE_FILE_ATS}"
     export SUIF_INSTALL_INSTALL_DIR="${SUIF_INSTALL_INSTALL_DIR_ATS}"
 
-
-env
- 
-
-   
     # Parameters - applySetupTemplate
     # $1 - Setup template directory, relative to <repo_home>/02.templates/01.setup
     applySetupTemplate "AT/1011/server/minimal-with-cu-on-postgresql" || exit 7
--- 03.test/MSR/1011/msr-ats-cu-db/scripts/containerEntrypointMSR.sh
+++ 03.test/MSR/1011/msr-ats-cu-db/scripts/containerEntrypointMSR.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-
+#tail -f /dev/null
 if [ ! -d ${SUIF_HOME} ]; then
     echo "SUIF_HOME variable MUST point to an existing local folder! Current value is ${SUIF_HOME}"
     exit 1
@@ -18,6 +18,7 @@ fi
 
 logFullEnv
 
+
 # If the installation is not present, do it now
 if [ ! -d "${SUIF_INSTALL_INSTALL_DIR}/IntegrationServer" ]; then
     logI "Starting up for the first time, setting up ..."
@@ -30,40 +31,6 @@ if [ ! -d "${SUIF_INSTALL_INSTALL_DIR}/IntegrationServer" ]; then
     # $1 - Setup template directory, relative to <repo_home>/02.templates/01.setup
     applySetupTemplate "MSR/1011/AdaptersSet1" || exit 6
 fi
-
-
-# If the DBC installation is not present, do it now
-if [ ! -f "${SUIF_INSTALL_INSTALL_DIR_DBC}/common/db/bin/dbConfigurator.sh" ]; then
-    logI "Database configurator is not present, setting up ..."
-
-    export SUIF_INSTALL_IMAGE_FILE="${SUIF_INSTALL_IMAGE_FILE_DBC4AT}"
-    export SUIF_PATCH_FIXES_IMAGE_FILE="${SUIF_PATCH_FIXES_IMAGE_FILE_DBC4AT}"
-    export SUIF_INSTALL_INSTALL_DIR="${SUIF_INSTALL_INSTALL_DIR_DBC}"
-
-    # Parameters - applySetupTemplate
-    # $1 - Setup template directory, relative to <repo_home>/02.templates/01.setup
-    applySetupTemplate "AT/1011/DBC4AT" || exit 6
-fi
-
-createDbComponents(){
-
-    logI "Eventually creating DB components..."
-    sleep 5 # this container is starting too quickly...
-    local p=`portIsReachable ${SUIF_DBSERVER_HOSTNAME} ${SUIF_DBSERVER_PORT}`
-    while [ $p -eq 0 ]; do
-        logI "Waiting for the database to come up, sleeping 5..."
-        sleep 5
-        p=`portIsReachable ${SUIF_DBSERVER_HOSTNAME} ${SUIF_DBSERVER_PORT}`
-        # TODO: add an maximum retry number
-    done
-
-    echo "portIsReachable ${SUIF_DBSERVER_HOSTNAME} ${SUIF_DBSERVER_PORT}"
-    export SUIF_INSTALL_INSTALL_DIR="${SUIF_INSTALL_INSTALL_DIR_DBC}"
-    # template specific parameters
-    applyPostSetupTemplate DBC/1011/postgresql-create
-}
-
-createDbComponents
 
 onInterrupt(){
 
@@ -85,6 +52,21 @@ trap "onKill" SIGKILL
 logI "Temp - pause"
 
 logFullEnv
+
+waitForATS(){
+
+    logI "Waiting for ATS to come up, sleeping 10..."
+    sleep 10 # this container is starting too quickly...
+    local p=`portIsReachable ${SUIF_INSTALL_DECLARED_HOSTNAME} ${SUIF_WMSCRIPT_IntegrationServerPort}`
+    while [ $p -eq 0 ]; do
+        logI "Waiting for ATS to come up, sleeping 10..."
+        sleep 10
+        p=`portIsReachable ${SUIF_INSTALL_DECLARED_HOSTNAME} ${SUIF_WMSCRIPT_IntegrationServerPort}`
+        # TODO: add an maximum retry number
+    done
+}
+
+waitForATS
 
 #tail -f /dev/null
 
