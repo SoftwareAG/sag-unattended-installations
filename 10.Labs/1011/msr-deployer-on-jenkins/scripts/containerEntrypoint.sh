434301a92404700c18528dcfba8ae9ef34aa6638
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2022-05-24 00:21:31 +0200

    Deployer on Jenkins on Centos7 Lab

---
+++ 10.Labs/1011/msr-deployer-on-jenkins/scripts/containerEntrypoint.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+env
+
+if [ ! -d ${SUIF_HOME} ]; then
+    echo "SUIF_HOME variable MUST point to an existing local folder! Current value is ${SUIF_HOME}"
+    exit 1
+fi
+
+# our configuration takes precedence in front of framework defaults, set it before sourcing the framework functions
+if [ ! -d "${SUIF_LOCAL_SCRIPTS_HOME}" ]; then
+    echo "Scripts folder not found: ${SUIF_LOCAL_SCRIPTS_HOME}"
+    exit 2
+fi
+
+# Source framework functions
+. "${SUIF_HOME}/01.scripts/commonFunctions.sh" || exit 4
+. "${SUIF_HOME}/01.scripts/installation/setupFunctions.sh" || exit 5
+
+logFullEnv
+
+logI "Starting Jenkins"
+
+/usr/local/bin/jenkins.sh &
+
+export Jenkins_PID=$!
+
+# If the installation is not present, do it now
+if [ ! -d "${SUIF_INSTALL_INSTALL_DIR}/IntegrationServer" ]; then
+    logI "Starting up for the first time, setting up ..."
+
+    # Parameters - applySetupTemplate
+    # $1 - Setup template directory, relative to <repo_home>/02.templates/01.setup
+    applySetupTemplate "MSR/1011/deployer" || exit 6
+fi
+
+onInterrupt(){
+    logI "Interrupted..."
+    logI "Killing Jenkins (PID=${Jenkins_PID})in background..."
+    kill $Jenkins_PID &
+
+    logI "Shutting down MSR..."
+    ${SUIF_INSTALL_INSTALL_DIR}/IntegrationServer/bin/shutdown.sh
+
+	exit 0 # managed expected exit
+}
+
+onKill(){
+	logW "Killed!"
+}
+
+trap "onInterrupt" SIGINT SIGTERM
+trap "onKill" SIGKILL
+
+rm ${SUIF_INSTALL_INSTALL_DIR}/IntegrationServer/bin/.lock
+
+${SUIF_INSTALL_INSTALL_DIR}/IntegrationServer/bin/server.sh & wait
+
+
+logI "MSR was shut down"
