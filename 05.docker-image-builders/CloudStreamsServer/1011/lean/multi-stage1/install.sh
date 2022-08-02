645060812098b071092cc25a2dbc6184017ace9e
Mihai Ungureanu (SAG) <108091043+miunsag@users.noreply.github.com>
2022-08-02 21:46:29 +0200

    CloudStreams multi-stage conatiner image builder no 1

---
+++ 05.docker-image-builders/CloudStreamsServer/1011/lean/multi-stage1/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export SUIF_TAG=${SUIF_TAG:-main}
+export SUIF_HOME=${SUIF_HOME:-"/tmp/SUIF_HOME"}
+export SUIF_TEMPLATE=${SUIF_TEMPLATE:-"CloudStreamsServer/1011/lean"}
+
+git clone -b "${SUIF_TAG}" --single-branch \
+  https://github.com/SoftwareAG/sag-unattended-installations.git \
+  "${SUIF_HOME}"
+
+. ${SUIF_HOME}/01.scripts/commonFunctions.sh
+. ${SUIF_HOME}/01.scripts/installation/setupFunctions.sh
+
+logI "SUIF env before installation:"
+env | grep SUIF_ | sort
+
+logI "Installing Product according to template ${SUIF_TEMPLATE}..."
+
+applySetupTemplate "${SUIF_TEMPLATE}"
+
+installResult=$?
+
+if [ "${installResult}" -ne 0 ]; then
+  logE "Installation failed, code ${installResult}"
+  exit 1
+fi
+
+logI "Product installation successful"
---
+++ 06.docker-image-builders-test/CloudStreamsServer/1011/multi-stage/test1/README.md
@@ -0,0 +1,9 @@
+# Test 1 of CloudStreamsServer 10.11 lean multi-stage build 1
+
+The purpose of this test is to validate the docker image builder `05.docker-image-builders/CloudStreamsServer/1011/lean/multi-stage1` locally.
+
+The test may be run immediately in Windows. Observe the commands in the `.bat` files to replicate in Linux.
+
+## Prerequisites
+
+The following local files must be present and declared in `setEnv.bat`. Rename `Example.setEnv.bat` to `setEnv.bat` and declare the file locations.
