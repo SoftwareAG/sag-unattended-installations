41836f21a5ce15c8de67930c3b23b7cd094f58b4
Mihai Ungureanu (SAG) <108091043+miunsag@users.noreply.github.com>
2022-10-14 17:52:43 +0200

    Forgot to save latest version for APIGW 10.11 DSOnly setEnvDefaults

--- 02.templates/01.setup/APIGateway/1011/DSOnly/setEnvDefaults.sh
+++ 02.templates/01.setup/APIGateway/1011/DSOnly/setEnvDefaults.sh
@@ -29,18 +29,8 @@ fi
 checkSetupTemplateBasicPrerequisites || exit $?
 
 # ------------------------------ Section 2 - check what the caller MUST provide, related to this specific template
-if [ -z "${SUIF_SETUP_TEMPLATE_YAI_LICENSE_FILE+x}" ]; then
-    logE "User must provide a valid license file in the SUIF_SETUP_TEMPLATE_YAI_LICENSE_FILE variable"
-    exit 21
-fi
-
-if [ ! -f "${SUIF_SETUP_TEMPLATE_YAI_LICENSE_FILE}" ]; then
-    logE "User must provide a valid license file in the SUIF_SETUP_TEMPLATE_YAI_LICENSE_FILE variable. Provided file ${SUIF_SETUP_TEMPLATE_YAI_LICENSE_FILE} not found!"
-    exit 22
-fi
 
 # ------------------------------ Section 3 - the caller MAY provide ( framework commons )
-
 export SUIF_INSTALL_DECLARED_HOSTNAME="${SUIF_INSTALL_DECLARED_HOSTNAME:-localhost}"
 export SUIF_INSTALL_INSTALL_DIR="${SUIF_INSTALL_INSTALL_DIR:-/opt/sag/products}"
 export SUIF_INSTALL_SPM_HTTP_PORT="${SUIF_INSTALL_SPM_HTTP_PORT:-9082}"
@@ -51,20 +41,12 @@ export SUIF_INSTALL_SPM_HTTPS_PORT="${SUIF_INSTALL_SPM_HTTPS_PORT:-9083}"
 export SUIF_WMSCRIPT_adminPassword="${SUIF_WMSCRIPT_adminPassword:-manage}"
 export SUIF_WMSCRIPT_CELHTTPPort="${SUIF_WMSCRIPT_CELHTTPPort:-9240}"
 export SUIF_WMSCRIPT_CELTCPPort="${SUIF_WMSCRIPT_CELTCPPort:-9340}"
-export SUIF_WMSCRIPT_IntegrationServerdiagnosticPort="${SUIF_WMSCRIPT_IntegrationServerdiagnosticPort:-9999}"
-export SUIF_WMSCRIPT_IntegrationServerPort="${SUIF_WMSCRIPT_IntegrationServerPort:-5555}"
-export SUIF_WMSCRIPT_IntegrationServersecurePort="${SUIF_WMSCRIPT_IntegrationServersecurePort:-5553}"
-export SUIF_WMSCRIPT_YAIHttpPort="${SUIF_WMSCRIPT_YAIHttpPort:-9072}"
-export SUIF_WMSCRIPT_YAIHttpsPort="${SUIF_WMSCRIPT_YAIHttpsPort:-9073}"
 
 # ------------------------------ Section 5 - Computed values
 
-SUIF_WMSCRIPT_integrationServerLicenseFiletext=$(urlencode "${SUIF_SETUP_TEMPLATE_YAI_LICENSE_FILE}")
-export SUIF_WMSCRIPT_integrationServerLicenseFiletext
-
 # ------------------------------ Section 6 - Constants
 
-export SUIF_CURRENT_SETUP_TEMPLATE_PATH="APIGateway/1011/default"
+export SUIF_CURRENT_SETUP_TEMPLATE_PATH="APIGateway/1011/DSOnly"
 
 logI "Template environment sourced successfully"
 logEnv4Debug
