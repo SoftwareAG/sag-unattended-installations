d22aeccfa919f345f73d44d9d144c6d8e5848e6f
Mihai Ungureanu (SAG) <108091043+miunsag@users.noreply.github.com>
2022-10-19 19:37:50 +0200

    Corrected syntax for POSIX compliance

--- 04.support/alpine-set-elk-prerequisites/scripts/setParams.sh
+++ 04.support/alpine-set-elk-prerequisites/scripts/setParams.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ $(id -u) -ne 0 ]]; then
+if [ $(id -u) -ne 0 ]; then
    echo "This script must be run as root" 
    exit 1
 fi
@@ -11,14 +11,14 @@ setVmMaxMapCount(){
     echo "Setting host kernel parameters for Elasticsearch"
 
     local p1=$(sysctl "vm.max_map_count" | cut -d " " -f 3)
-    if [[ ! $p1 -lt $c1 ]]; then
+    if [ ! $p1 -lt $c1 ]; then
         echo "vm.max_map_count is adequate ($p1)"
     else
         echo "vm.max_map_count is NOT adequate ($p1), setting it to minimum required value of $c1"
         sysctl -w "vm.max_map_count=$c1" || return 2
         echo "vm.max_map_count set, checking effectiveness"
         local p2=$(sysctl "vm.max_map_count" | cut -d " " -f 3)
-        if [[ ! $p2 -lt $c1 ]]; then
+        if [ ! $p2 -lt $c1 ]; then
             echo "vm.max_map_count correctly set"
         else
             echo "vm.max_map_count was NOT set, cannot continue"
