beefa4f8cc5b72de9470f0573924789f196e1b1d
miunsag <108091043+miunsag@users.noreply.github.com>
2023-11-27 16:56:21 +0100

    Added an msr lean builder with support patch example

---
+++ 06.docker-image-builders-test/MSR/1015/msr-with-sp-custom-builder-03/runTest.bat
@@ -0,0 +1,28 @@
+@echo off
+
+call .\setEnv.bat
+
+md build_context
+
+copy ..\..\..\..\05.docker-image-builders\MSR\1015\msr-with-sp-custom-builder-03\Dockerfile build_context\
+copy ..\..\..\..\05.docker-image-builders\MSR\1015\msr-with-sp-custom-builder-03\*.sh build_context\
+
+COPY "%INSTALLER_BIN%" build_context\installer.bin
+COPY "%SUM_BOOTSTRAP_BIN%" build_context\sum-bootstrap.bin
+COPY "%PRODUCTS_ZIP%" build_context\products.zip
+COPY "%FIXES_ZIP%" build_context\fixes.zip
+COPY "%SP_ZIP%" build_context\sp.zip
+COPY "%MSR_LICENSE_XML%" build_context\msr-license.xml
+
+
+cd build_context
+
+echo SP_ID=%SP_ID%
+
+docker build -t msr-1015-with-sp-custom-builder-01-test-01 --build-arg __sp_id=%SP_ID% --no-cache .
+
+cd ..
+
+del /q build_context\*
+
+rd /q build_context
